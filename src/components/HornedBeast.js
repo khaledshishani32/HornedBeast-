710d09f99d4c9f776038962395a409a307be0222
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-07 23:56:05 +0300

    finish lab02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -4,13 +4,36 @@ import React from "react";
 
 class HornedBeast extends React.Component {
 
+  constructor(props) {
+    // we are passing the props object as an argument to our parent React Component Class 
+    super(props)
+
+    // a react standard of saving information related to the class component
+    // we call those information as states (conditions) to describe the state/ condition of the component.
+    this.state = {
+        childName: this.props.name,
+        vote: 0
+    }
+}
+
+requestVote = () => {
+    // the request money child function will be invoking or calling the passed giveMoney function from the parent component
+    this.props.giveVote();
+    let tempValue = this.state.vote;
+    this.setState({
+        vote: tempValue += 1
+    })
+}
+
+
   render() {
     return (
       <div>
 
         <div>
           <h2>{this.props.title}</h2>
-          <img src={this.props.img} alt={this.props.title} ></img>
+          <p>⭐{this.state.vote}</p>
+          <img onClick={this.requestVote} style={{ width: "200px", height: "200px" }} src={this.props.img} alt={this.props.title} ></img>
           <p>{this.props.description}</p>
         </div>
 
