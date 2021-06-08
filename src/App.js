d26fbb59055ed06095175a050e6b97cb72fb95bb
khaled al shishani <khaled.shishani32@gmail.com>
2021-06-09 00:19:20 +0300

    lab03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import SelectedBeast from "./SelectedBeast";
+
 
 
 
@@ -9,34 +13,69 @@ class HornedBeast extends React.Component {
     super(props)
 
     this.state = {
-       
-        vote: 0
-    }
-}
 
-requestVote = () => {
- 
+      vote: 0
+    }
+  }
+
+  requestVote = () => {
+
     this.props.giveVote();
     let tempValue = this.state.vote;
     this.setState({
-        vote: tempValue += 1
+      vote: tempValue += 1
     })
-}
+  }
 
+  
 
   render() {
     return (
+      // <div>
+
+      //   <div>
+      //     <h2>{this.props.title}</h2>
+      //     <p>⭐{this.state.vote}</p>
+      //     <img onClick={this.requestVote} style={{ width: "200px", height: "200px" }} src={this.props.img} alt={this.props.title} ></img>
+      //     <p>{this.props.description}</p>
+      //   </div>
+
+
+      // </div>
       <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img onClick={this.requestVote}  variant="top" style={{ width: "200px", height: "200px" }} src={this.props.img} />
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>
+              {this.props.description}
+            </Card.Text>
+            <Card.Text>
+              ⭐ {this.state.vote}
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-        <div>
-          <h2>{this.props.title}</h2>
-          <p>⭐{this.state.vote}</p>
-          <img onClick={this.requestVote} style={{ width: "200px", height: "200px" }} src={this.props.img} alt={this.props.title} ></img>
-          <p>{this.props.description}</p>
-        </div>
+          
 
-
+         
       </div>
+
+      //   <Row xs={1} md={4} className="g-4">
+      //   {Array.from({ length: 4 }).map((_, idx) => (
+      //     <Col>
+      //       <Card>
+      //         <Card.Img variant="top"  style={{ width: "200px", height: "200px" }} src={this.props.img} />
+      //         <Card.Body>
+      //           <Card.Title>{this.props.title}</Card.Title>
+      //           <Card.Text>
+      //           {this.props.description}
+      //           </Card.Text>
+      //         </Card.Body>
+      //       </Card>
+      //     </Col>
+      //   ))}
+      // </Row>
 
     )
 
